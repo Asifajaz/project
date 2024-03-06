ce64dcaeefb0a2bc167bab8fc0e3f4e20357b6e8
Asifajaz <159148615+Asifajaz@users.noreply.github.com>
2024-03-06 16:09:48 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+// // // data type (PREMITIVE)
+// // // string
+// // // number 
+// // // boolran
+// // // null
+// // // url
+// // // first class
+// // // let fullname = "hello world"
+// // // console.log (fullname)
+// // // 2nd class
+// // // let a : number = 10;
+// // // let b : number = 20;
+// // // 3rd class
+// // // console.log ("a + b=", a + b );
+// // // console.log ("a - b=", a - b );
+// // // console.log ("a * b=", a * b );
+// // // console.log ("a / b=", a / b );
+// // // console.log ("a % b=", a % b );
+// // // 4 class
+// // // let massage : string = 'Asif ! ;'
+// // // console.log (massage)
+// // // massage = '133'
+// // // console.log (massage)
+// // // let fname : string = "asif";
+// // // console.log ('good day , ' , fname, '!')
+// // // let fname : string = "asif";
+// // // console.log ('good day , ' +  fname + '!')
+// // // let uname : string = 'ASIF '; // STRING
+// // // let age : number = 24 ; // number
+// // // let isStudent : Boolean = false; // boolean
+// // // let randomvalue : any = 'https://www.fiverr.com/asifajazmugahl?up_rollout=true'; // any
+// // //{string}
+// // // let a : string = ' helllo'
+// // // console.log(a)
+// // // // {number}
+// // // let b : number = ' 21'
+// // // console.log(b)
+// // // // {boolean}
+// // // let c : boolean = true
+// // // console.log(c)
+// // // //{null}
+// // // let d : null = 'null'
+// // // console.log(d)
+// // // class 5
+// // // cases
+// // // camelCase
+// // // snake_case
+// // // PascalCase
+// // // let userName = 'asif'  
+// // // let user_name = 'asif'
+// // // let UserName = 'asif'
+// // // let isUserExist = 'asif'
+// // // let is_user_exist = 'asif'
+// // // let IsUserExist = 'asif'
+// // // array = []
+// // let students : string [] = ['ali', 'asif', 'wasif']
+// // let firstStudentName = students [0]
+// // console.log(firstStudentName)
+// // console.log(students [5])
+// // console.log(students [6])
+// // console.log(students [1], students [2] )
+//update karny k lia
+var uName = 'asif';
+uName = 'daniyal';
+var fruits = ['apple', 'mango', 'banana'];
+fruits[1] = 'grapes';
+console.log(fruits);
+// // exexcise
+// let uName : number [] = [1,2,3,4,5,6]
+// console.log(uName)
+// let uBoolean:boolean [] = [true,false,true,false]
+// console.log (uBoolean)
